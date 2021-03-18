136d51c3adc0296dff6d35465ddb5cf3c4dbc6bc
Rajat <namdevrajat77@gmail.com>
2021-03-18 13:11:20 +0530

    add new file

---
+++ program2.py
@@ -0,0 +1,11 @@
+'''Ask the user for a number. 
+Depending on whether the number
+ is even or odd, print out an appropriate 
+ message to the user'''
+
+num=eval(input("enter a number"))
+if (num%2==0):
+    print("this is an even number")
+else:
+    print("this is an odd number")
+    
